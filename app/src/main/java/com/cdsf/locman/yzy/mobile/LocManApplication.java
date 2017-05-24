249370379d17ac88c97498306f1414b5852ad29b
吴文亮 <wuwenliang@ACA8016D.ipt.aol.com>
2017-05-24 11:30:04 +0800

    LocmanRunner

--- app/src/main/java/com/cdsf/locman/yzy/mobile/LocManApplication.java
+++ app/src/main/java/com/cdsf/locman/yzy/mobile/LocManApplication.java
@@ -3,6 +3,8 @@ import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
+import java.util.Properties;
+
 /**
  * Created by wuwenliang on 2017/5/17.
  */
@@ -12,5 +14,8 @@ public class LocManApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ARouter.init(this);
+        Properties properties=new Properties();
+        String moduleValue=properties.getProperty("module");
+        System.out.println("---wuwenliangJJPPUUIIOOMMMM:::"+moduleValue);
     }
 }
